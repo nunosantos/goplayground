e3aa5b0dd82d62f685184d9fcc9e67a636c51480
Nuno Santos <Nuno.santos@lego.com>
2023-09-15 22:01:44 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+func handle() {
+	
+}
+
+func main() {
+
+}
+
+/*
+func main() {
+	var username = "Nuno"
+	const numberOfTickets = 50
+	var remainingTickets = 30
+	fmt.Printf("Hello %v and welcome to the booking system\n", username)
+	fmt.Printf("You can buy a number of %v tickets and ticket remaining are %v.\n", numberOfTickets, remainingTickets)
+}
+*/
